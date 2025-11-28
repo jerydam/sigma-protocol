f7a7daca86ffcf029da8365f5523b013558020e5
jerydam <jerydan148@gmail.com>
2025-11-28 12:29:30 +0100

    functional

--- frontend/.next/dev/static/chunks/app_create_page_tsx_09717ff4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3551e92e._.js",
-  "static/chunks/node_modules__pnpm_a078cb8a._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/app_page_tsx_09717ff4._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_16b4682f._.js"
-],
-    source: "dynamic"
-});
